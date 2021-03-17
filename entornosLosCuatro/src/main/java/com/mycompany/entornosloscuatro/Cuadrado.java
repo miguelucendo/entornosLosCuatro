f29551294f1b13c5db7c3765d8d698763a567d5d
Miguel <Miguel@MIGUEL>
2021-03-18 00:59:56 +0100

    Añadida la clase Cuadrado

---
+++ entornosLosCuatro/src/main/java/com/mycompany/entornosloscuatro/Cuadrado.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.entornosloscuatro;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Cuadrado {
+ 
+    private int base, altura;
+    
+    public Cuadrado(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    
+}

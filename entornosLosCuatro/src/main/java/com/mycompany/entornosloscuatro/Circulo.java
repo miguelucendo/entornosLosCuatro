98ea9abac8c93679aa551879ddee41e51fcf61e4
Miguel <Miguel@MIGUEL>
2021-03-18 01:04:19 +0100

    Añadida la clase Circulo

---
+++ entornosLosCuatro/src/main/java/com/mycompany/entornosloscuatro/Circulo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.entornosloscuatro;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Circulo {
+ 
+    private int radio;
+    
+    public Circulo(int radio) {
+        this.radio = radio;
+    }
+    
+}
